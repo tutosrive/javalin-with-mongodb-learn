687f75ca8bb9d3b4cf2be9570689b978a9bfe3d0
tutosrive <tutosrivegamer@gmail.com>
2026-05-21 20:50:10 -0500

    Add first very simple test running Javalin

---
+++ app/src/main/java/nosql/server/JavalinServer.java
@@ -0,0 +1,34 @@
+/*
+ * server - Just test-learn with javalin and in future with MongoDB
+ * Copyright 2026 - 2026 tutosrive
+ * Licence: GPL-3
+ * More information: https://github.com/tutosrive/javalin-with-mongodb-learn/blob/main/LICENSE
+ * Author: tutosrive (tutosrive@Dev2Forge.software)
+ * 
+ * File: \JavalinServer.java
+ * Created: Thursday, 21st May 2026 8:22:27 pm
+ * -----
+ * Last Modified: Thursday, 21st May 2026 8:30:54 pm
+ * Modified By: tutosrive (tutosrive@Dev2Forge.software)
+ * -----
+ */
+
+package nosql.server;
+
+import io.javalin.Javalin;
+
+public class JavalinServer {
+    public Javalin app;
+
+    public JavalinServer() {
+        this(7070);
+    }
+
+    public JavalinServer(int port) {
+        this.app = Javalin.create(config -> {
+            config.routes.get("/", ctx -> {
+                ctx.result("This is a very simple test!");
+            });
+        }).start(port);
+    }
+}
