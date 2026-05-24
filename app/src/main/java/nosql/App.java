301a1b715e7129a1f064fea63bfbe34504392fa3
tutosrive <tutosrivegamer@gmail.com>
2026-05-23 21:12:27 -0500

    Add first sample test trying connect MongoDB ... Work correctly! so easy

--- app/src/main/java/nosql/App.java
+++ app/src/main/java/nosql/App.java
@@ -8,17 +8,39 @@
  * File: \App.java
  * Created: Thursday, 21st May 2026 7:48:45 pm
  * -----
- * Last Modified: Thursday, 21st May 2026 8:49:07 pm
+ * Last Modified: Saturday, 23rd May 2026 9:10:58 pm
  * Modified By: tutosrive (tutosrive@Dev2Forge.software)
  * -----
  */
 
 package nosql;
 
-import nosql.server.JavalinServer;
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+
+// import nosql.server.JavalinServer;
 
 public class App {
     public static void main(String[] args) {
-        new JavalinServer();
+        // new JavalinServer();
+        String connectionString = "mongodb://localhost:27017/";
+
+        try (MongoClient mgClient = MongoClients.create(connectionString)) {
+            MongoDatabase db = mgClient.getDatabase("dbusertest");
+            MongoCollection<Document> testCollection = db.getCollection("users");
+
+            Document doc1 = testCollection.find(eq("email", "zachary.lo@example.com")).first();
+
+            if (!doc1.isEmpty()) {
+                System.out.println(doc1.toJson());
+            } else {
+                System.out.println("Don't match a document!");
+            }
+        }
     }
 }
